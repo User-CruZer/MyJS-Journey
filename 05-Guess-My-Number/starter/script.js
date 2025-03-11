59fc3a386077d966849a539f395090a46e7e6810
User-CruZer <ahmadzakialbadri@gmail.com>
2025-03-11 15:29:07 +0700

    handling click events

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 console.log(document.querySelector(".message").textContent);
 document.querySelector(".message").textContent = "🎉 Correct answer!"
 console.log(document.querySelector(".message").textContent);
@@ -9,3 +9,13 @@ document.querySelector(".score").textContent = 10;
 
 document.querySelector(".guess").value = 23;
 console.log(document.querySelector(".guess").value);
+*/
+
+document.querySelector(".check").addEventListener("click", function() {
+    const x = Number(document.querySelector(".guess").value);
+    console.log(x, typeof(x));
+
+    if(!x) {
+        document.querySelector(".message").textContent = "🗿 No Number!"
+    }
+})
