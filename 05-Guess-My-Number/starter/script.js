6954cc8d2b09af4f545c4317adab9a95e023b75e
User-CruZer <ahmadzakialbadri@gmail.com>
2025-03-11 16:24:31 +0700

    implementing the game logic

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -11,11 +11,37 @@ document.querySelector(".guess").value = 23;
 console.log(document.querySelector(".guess").value);
 */
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(".number"). textContent = secretNumber;
+let score = 20;
+
+
 document.querySelector(".check").addEventListener("click", function() {
     const x = Number(document.querySelector(".guess").value);
     console.log(x, typeof(x));
 
     if(!x) {
-        document.querySelector(".message").textContent = "🗿 No Number!"
+        document.querySelector(".message").textContent = "🗿 No number!";
+    } else if (x === secretNumber) {
+        document.querySelector(".message").textContent = `${secretNumber} is the correct number`
+    } else if(x > secretNumber) {
+        if (score > 1) {
+            document.querySelector(".message").textContent = `📈 Number is too High!`;
+        score--;
+        document.querySelector(".score").textContent = score;
+        } else {
+            document.querySelector(".message").textContent = `You have lost the game`;
+            document.querySelector(".score").textContent = 0;
+        }
+    } else if(x < secretNumber) {
+        if (score > 1) {
+            document.querySelector(".message").textContent = `📉 Number is too Low!`;
+            score--;
+            document.querySelector(".score").textContent = score;
+            } else {
+                document.querySelector(".message").textContent = `You have lost the game`;
+                document.querySelector(".score").textContent = 0;
+            
+        }
     }
-})
+});
