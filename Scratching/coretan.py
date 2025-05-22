aeee27df0e47fa1dfc9d722daa4d2547733c466d
User-CruZer <ahmadzakialbadri@gmail.com>
2025-05-22 07:51:01 +0700

    Adding some new material for javascript

--- Scratching/coretan.py
+++ Scratching/coretan.py
@@ -1,35 +1,48 @@
-# Comparing if else statement from JavaScript
-joko = 60
-if joko <= 30 and joko >= 20:
-    print("consider yourself as still young")
-elif joko >= 40 or joko <= 50:
-    print("You're old")
+# # Comparing if else statement from JavaScript
+# joko = 60
+# if joko <= 30 and joko >= 20:
+#     print("consider yourself as still young")
+# elif joko >= 40 or joko <= 50:
+#     print("You're old")
 
-# Comparing list and array from JavaScript
-me = ["zaki", 19]
-brother = ["yusuf", 26]
-print(len(brother))
-print(brother[0])
+# # Comparing list and array from JavaScript
+# me = ["zaki", 19]
+# brother = ["yusuf", 26]
+# print(len(brother))
+# print(brother[0])
 
-# append methods:
-me.append(2006)
-print(f"1. {me}")
+# # append methods:
+# me.append(2006)
+# print(f"1. {me}")
 
-# pop methods:
-me.pop(0)
-print(f"2. {me}")
+# # pop methods:
+# me.pop(0)
+# print(f"2. {me}")
 
-# insert methods:
-me.insert(0, "joko")
-print(f"3. {me}")
+# # insert methods:
+# me.insert(0, "joko")
+# print(f"3. {me}")
 
-# looping in python
-for i in range(4):
-    i = "joko"
-    print(i)
+# # looping in python
+# for i in range(4):
+#     i = "joko"
+#     print(i)
 
-for i in range(5):
-    print(i + 1)
+# for i in range(5):
+#     print(i + 1)
 
-joko = input("insert anything here: ").capitalize()
-print(joko)
+# joko = input("insert anything here: ").capitalize()
+# print(joko)
+
+# joko = 10
+# if (joko == 20):
+#     print("nice")
+
+def solveMeFirst(a,b):
+    return a + b
+    
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
