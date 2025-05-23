9f67be9753bca32734bd81df5b84ec663494f5c0
User-CruZer <ahmadzakialbadri@gmail.com>
2025-05-23 09:04:26 +0700

    indepth understanding of scope chain

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,35 @@
 'use strict';
+
+function calcAge(birthyear) {
+    const age = 2037 - birthyear;
+    
+    function printAge() {
+        const output = `${firstname}, you are ${age} years old in 2037, and you were born in ${birthyear}`;
+        var milennial = true;
+        console.log(output);
+
+        if(birthyear >= 1981 && birthyear <= 1999) {
+            // const firstname = "Ojak";
+            console.log(`${firstname}, You are a milennial`);
+        } else if (birthyear >= 2000 && birthyear <= 2010) {
+            // const firstname = "Ojak";
+            console.log(`${firstname}, You are a gen z`);
+        }
+
+        function add(a, b) {
+            return a + b
+        }
+    }
+    printAge();
+
+    return age;
+}
+
+const firstname = "Zaki";
+// calcAge(2006);
+// console.log(firstname, calcAge(2006));
+
+document.getElementById("btn1").addEventListener("click", function() {
+    console.log(firstname);
+    calcAge(2006);
+})
