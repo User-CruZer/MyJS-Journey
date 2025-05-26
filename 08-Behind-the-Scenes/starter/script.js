a3b2d4208b2cbc85f9c692ca688453baa250b300
User-CruZer <ahmadzakialbadri@gmail.com>
2025-05-26 07:43:25 +0700

    learning about hoisting in javascript

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -35,3 +35,57 @@ document.getElementById("btn1").addEventListener("click", function() {
     console.log(firstname);
     calcAge(2006);
 })
+
+
+
+// Hoisting
+
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+
+
+// Variables
+var me = "Zaki";
+let job = "unemployed";
+const year = 2006;
+
+// functions
+console.log(addDecl(2,3));
+// console.log(addExpr(2,3));
+// console.log(addArr(2,3));
+
+function addDecl(a, b) {
+    return a + b
+}
+
+const addExpr = function(a, b) {
+    return a + b
+}
+
+const addArr = (a, b) => a + b;
+
+
+// example 
+let carts = 12
+if(!carts) {
+    deleteShoppingCart()
+} else if (carts) {
+    console.log("You got some carts to pay")
+}
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+    console.log("Your shopping cart is clear");
+}
+
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
